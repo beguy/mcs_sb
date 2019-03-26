9353bd2afbaf2fc8c5828db958c8af1847e92c0e
Viktor <13942538+beguy@users.noreply.github.com>
2019-03-26 13:28:07 +0300

    task1 and 2 done

--- module2/src/main/java/Main.java
+++ module2/src/main/java/Main.java
@@ -3,11 +3,14 @@ import java.util.List;
 public class Main {
     // task1
     public static int uniqueNumbers(List<? extends Number> numberList) {
-
+        return (int) numberList.stream().distinct().count();
     }
 
     // task2
     public static boolean xor(List<List<Boolean>> matrix) {
-
+        return matrix.stream()
+                .parallel()
+                .flatMap(row -> row.stream())
+                .reduce(false, (a, b) -> a ^ b);
     }
 }
