b6cc1f60f39227c1d8a74021c41cf637fb009c66
Viktor <13942538+beguy@users.noreply.github.com>
2019-05-10 11:42:51 +0300

    client controller + view

---
+++ module6/src/main/java/com/github/beguy/module6/client/ClientController.java
@@ -0,0 +1,59 @@
+package com.github.beguy.module6.client;
+
+import com.github.beguy.module6.accountType.AccountTypeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class ClientController {
+    @Autowired
+    private ClientDao clientDao;
+
+    @Autowired
+    private AccountTypeDao accountTypeDao;
+
+    @GetMapping("/clients")
+    public String showAll(Model model) {
+        model.addAttribute("accountTypes", accountTypeDao.findAll());
+        model.addAttribute("clients", clientDao.findAll());
+        return "/client/all";
+    }
+
+    @GetMapping("/client/{id}/delete/")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        clientDao.delete(id);
+        return "redirect:/clients";
+    }
+
+    @PostMapping("/client/add")
+    public String addClient(@Valid Client client, @ModelAttribute(value = "accountTypeId") long accountTypeId, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("error", "can't add this client " + client);
+            return "/error";
+        }
+
+        client.setAccountType(accountTypeDao.findById(accountTypeId));
+        clientDao.save(client);
+        return "redirect:/clients";
+    }
+
+    @PostMapping("/client/edit")
+    public String updateClient(@Valid Client client, @ModelAttribute(value = "accountTypeId") long accountTypeId, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("error", "can't edit this client " + client);
+            return "/error";
+        }
+
+        client.setAccountType(accountTypeDao.findById(accountTypeId));
+        clientDao.update(client);
+        return "redirect:/clients";
+    }
+}
---
+++ module6/src/main/resources/static/js/helpers.js
@@ -0,0 +1,26 @@
+function synchronizeSelectId() {
+    var selects = document.getElementsByClassName("selectWithSynchronizedId");
+    selects = Array.from(selects); //convert to array
+
+    selects.forEach(select = > {
+        //get id from classname: selected=#id
+        let classNames = Array.from(select.classList);
+    let selectedClassNamePosition = classNames.findIndex(className = > className.includes("selected")
+)
+    ;
+    let id = classNames[selectedClassNamePosition].split("=")[1];
+
+    //find option position from value
+    let options = Array.from(select.options);
+    let optionIndex = options.map(option = > option.value
+)
+.
+    findIndex(value = > (value == id) ? true : false
+)
+    ;
+
+    //set selected to finded position
+    select.selectedIndex = optionIndex;
+})
+    ;
+}
