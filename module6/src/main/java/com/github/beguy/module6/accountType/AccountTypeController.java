518cb8a873f68b9b8f201ab88ffd94eec54c5d17
Viktor <13942538+beguy@users.noreply.github.com>
2019-05-11 13:05:41 +0300

    Account Type Controller + full front end 4 all.

---
+++ module6/src/main/java/com/github/beguy/module6/accountType/AccountTypeController.java
@@ -0,0 +1,57 @@
+package com.github.beguy.module6.accountType;
+
+import com.github.beguy.module6.bank.BankDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class AccountTypeController {
+    @Autowired
+    private AccountTypeDao accountTypeDao;
+    @Autowired
+    private BankDao bankDao;
+
+    @GetMapping("/accountTypes")
+    public String showAll(Model model) {
+        List<AccountType> accountTypes = accountTypeDao.findAll();
+        model.addAttribute("accountTypes", accountTypes);
+        model.addAttribute("banks", bankDao.findAll());
+        return "/accountType/all";
+    }
+
+    @GetMapping("/accountType/{id}/delete/")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        accountTypeDao.delete(id);
+        return "redirect:/accountTypes";
+    }
+
+    @PostMapping("/accountType/add")
+    public String addAccountType(@Valid AccountType accountType, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("error", "can't add this accountType " + accountType);
+            return "/error";
+        }
+
+        accountTypeDao.save(accountType);
+        return "redirect:/accountTypes";
+    }
+
+    @PostMapping("/accountType/edit")
+    public String updateAccountType(@Valid AccountType accountType, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("error", "can't delete this accountType " + accountType);
+            return "/error";
+        }
+
+        accountTypeDao.update(accountType);
+        return "redirect:/accountTypes";
+    }
+}
--- module6/src/main/resources/static/js/helpers.js
+++ module6/src/main/resources/static/js/helpers.js
@@ -2,15 +2,15 @@ function synchronizeSelectId() {
     var selects = document.getElementsByClassName("selectWithSynchronizedId");
     selects = Array.from(selects); //convert to array
 
-    selects.forEach(select => {
+    selects.forEach(select=>{
         //get id from classname: selected=#id
         let classNames = Array.from(select.classList);
-        let selectedClassNamePosition = classNames.findIndex(className => className.includes("selected"));
+        let selectedClassNamePosition = classNames.findIndex(className=>className.includes("selected"));
         let id = classNames[selectedClassNamePosition].split("=")[1];
         //find option position from value
         let options = Array.from(select.options);
-        let optionIndex = options.map(option => option.value)
-                                 .findIndex(value = > (value == id) ? true : false);
+        let optionIndex = options.map(option=>option.value)
+                                 .findIndex(value=>(value == id) ? true : false);
         //set selected to finded position
         select.selectedIndex = optionIndex;
     });
