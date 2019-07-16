3c3f22f81a62fcfa061bbcadfad500c0a7198f69
Viktor <13942538+beguy@users.noreply.github.com>
2019-07-16 20:41:37 +0300

    Evict Cache for task 7.1

--- module6-7/src/main/java/com/github/beguy/module6/client/ClientRepository.java
+++ module6-7/src/main/java/com/github/beguy/module6/client/ClientRepository.java
@@ -1,6 +1,8 @@
 package com.github.beguy.module6.client;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@CacheEvict
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
