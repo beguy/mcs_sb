f87a7f92b6765a9498f3ae0880fbfaaad8052e9d
Viktor <13942538+beguy@users.noreply.github.com>
2019-07-16 20:33:00 +0300

    YAGNI!

--- module6-7/src/main/java/com/github/beguy/module6/accountType/AccountTypeController.java
+++ module6-7/src/main/java/com/github/beguy/module6/accountType/AccountTypeController.java
@@ -4,12 +4,8 @@ import com.github.beguy.module6.bank.BankRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
 
 @Controller
 public class AccountTypeController {
@@ -24,22 +20,5 @@ public class AccountTypeController {
         model.addAttribute("accountTypeInfo", accountTypeRepository.getAllInfo());
         model.addAttribute("banks", bankRepository.findAll());
         return "/accountTypes";
-    }
-
-    @GetMapping("/accountType/{id}/delete/")
-    public String delete(@PathVariable("id") long id, Model model) {
-        accountTypeRepository.deleteById(id);
-        return "redirect:/accountTypes";
-    }
-
-    @PostMapping("/accountType/save")
-    public String save(@Valid AccountType accountType, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            model.addAttribute("error", "can't add this accountType " + accountType);
-            return "/error";
-        }
-
-        accountTypeRepository.save(accountType);
-        return "redirect:/accountTypes";
     }
 }
--- module6-7/src/main/java/com/github/beguy/module6/bank/BankController.java
+++ module6-7/src/main/java/com/github/beguy/module6/bank/BankController.java
@@ -3,12 +3,7 @@ package com.github.beguy.module6.bank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 
 @Controller
 public class BankController {
@@ -19,22 +14,5 @@ public class BankController {
     public String showAll(Model model) {
         model.addAttribute("banks", bankRepository.findAll());
         return "/banks";
-    }
-
-    @GetMapping("/bank/{id}/delete/")
-    public String delete(@PathVariable("id") long id, Model model) {
-        bankRepository.deleteById(id);
-        return "redirect:/banks";
-    }
-
-    @PostMapping("/bank/save")
-    public String save(@Valid Bank bank, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            model.addAttribute("error", "can't add this bank " + bank);
-            return "/error";
-        }
-
-        bankRepository.save(bank);
-        return "redirect:/banks";
     }
 }
