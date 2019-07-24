b4f01719c410f30bdf596623f6ab38515c9fc457
Viktor <13942538+beguy@users.noreply.github.com>
2019-07-24 17:46:36 +0300

    Forget make / path available

--- module6-7/src/main/java/com/github/beguy/module6/core/security/WebSecurityConfig.java
+++ module6-7/src/main/java/com/github/beguy/module6/core/security/WebSecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        String[] resources = new String[]{"/login", "/error", "/banks", "/clients",
+        String[] resources = new String[]{"/login", "/", "/error", "/banks", "/clients",
                 "/accountTypes", "/css/**", "/js/**", "favicon.ico"};
 
         http
