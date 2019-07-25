e0c2bbf75173adff748466e5f9d7b81140f2cd67
Viktor <13942538+beguy@users.noreply.github.com>
2019-07-25 12:03:29 +0300

    Add profiles. Fix test.

--- module6-7/src/main/java/com/github/beguy/module6/Application.java
+++ module6-7/src/main/java/com/github/beguy/module6/Application.java
@@ -3,32 +3,14 @@ package com.github.beguy.module6;
 import com.github.beguy.module6.core.security.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
 @Import({WebSecurityConfig.class})
-@EnableJpaRepositories(basePackages = "com.github.beguy.module6")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-
-    @Bean
-    @Profile("test")
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.HSQL)
-                //.addScript("classpath:some.sql")
-                .build();
     }
 }
--- module6-7/src/test/java/com/github/beguy/module6/account/AccountRepositoryTest.java
+++ module6-7/src/test/java/com/github/beguy/module6/account/AccountRepositoryTest.java
@@ -4,20 +4,20 @@ package com.github.beguy.module6.account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
-@DataJpaTest
+@SpringBootTest
 public class AccountRepositoryTest {
     @Autowired
     AccountRepository accountRepository;
@@ -42,7 +42,10 @@ public class AccountRepositoryTest {
         List<Account> accounts = Arrays.asList(accountBeforeDate, accountInDate, accountAfterDate, anotherAccountAfterDate);
         accounts.forEach(account -> account.setAccountDate(startedDate));
         accountRepository.saveAll(accounts);
-        List<Account> finded = accountRepository.findAllByServiceFeeDate(currentDate);
-        assertThat(finded, is(Arrays.asList(accountBeforeDate, accountInDate)));
+        List<Long> findedIds = accountRepository.findAllByServiceFeeDate(currentDate)
+                .stream().map(Account::getId)
+                .collect(Collectors.toList());
+        List<Long> expectedIds = Arrays.asList(accountBeforeDate.getId(), accountInDate.getId());
+        assertTrue(expectedIds.equals(findedIds));
     }
 }
